cc72490e12711eb573262781e4a1ef5f827e602f
吳哲維(Wu_Che_Wei) <s1090374@gm.pu.edu.tw>
2023-11-21 15:08:00 +0800

    Update ApiService.cs

--- NewsApp/Services/ApiService.cs
+++ NewsApp/Services/ApiService.cs
@@ -14,7 +14,7 @@ namespace NewsApp.Services
         public async Task<Root> GetNews(string categoryName)
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetStringAsync($"https://gnews.io/api/v4/top-headlines?category=general&lang=en&country={categoryName.ToLower()}&apikey=d5e56396c7628cc8e6accaced8a3ba80");
+            var response = await httpClient.GetStringAsync($"https://gnews.io/api/v4/top-headlines?country=tw&category={categoryName.ToLower()}&apikey=bc4ba7e4bb11f868a7eed90948baf77b");
             return JsonConvert.DeserializeObject<Root>(response);
         }
     }
